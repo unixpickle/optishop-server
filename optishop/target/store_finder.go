8f68aa735035692b80a5cdfb2d5bb8fb99901171
Alex Nichol <unixpickle@gmail.com>
2019-07-12 14:45:04 -0400

    drafted store search

--- optishop/target/store_finder.go
+++ optishop/target/store_finder.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pkg/errors"
 )
 
+// A GeocodesResult stores the result of a Geocodes call.
 type GeocodesResult struct {
 	Place     string `json:"place"`
 	Locale    string `json:"locale"`
@@ -25,10 +26,64 @@ type GeocodesResult struct {
 	} `json:"locations"`
 }
 
+// LocationInfo stores information about a store location
+// from a SearchStores query.
+type LocationInfo struct {
+	LocationID      int    `json:"location_id"`
+	TypeCode        string `json:"type_code"`
+	TypeDescription string `json:"type_description"`
+	LocationNames   []struct {
+		NameType string `json:"name_type"`
+		Name     string `json:"name"`
+	}
+	Address struct {
+		AddressLine1 string `json:"address_line1"`
+		City         string `json:"city"`
+		County       string `json:"county"`
+		Region       string `json:"region"`
+		State        string `json:"state"`
+		PostalCode   string `json:"postal_code"`
+	}
+	Distance     float64 `json:"distance"`
+	DistanceUnit string  `json:"distance_unit"`
+}
+
+// Name gets the human-readable name of the store.
+func (l *LocationInfo) Name() string {
+	for _, name := range l.LocationNames {
+		if name.NameType == "Proj Name" {
+			return name.Name
+		}
+	}
+	if len(l.LocationNames) > 0 {
+		return l.LocationNames[0].Name
+	}
+	return fmt.Sprintf("Location #%d", l.LocationID)
+}
+
 // SearchStores finds stores based on a query, e.g. a zip
 // code, address, state, etc.
-func (c *Client) SearchStores(query string) {
-	// https://redsky.target.com/v3/stores/nearby/georgia?key=eb2551e4accc14f38cc42d32fbc2b2ea&limit=20&within=100&unit=mile
+func (c *Client) SearchStores(query string) ([]*LocationInfo, error) {
+	q := url.Values{}
+	q.Add("key", c.Key())
+	q.Add("limit", "20")
+	q.Add("within", "100")
+	q.Add("unit", "mile")
+	u := "https://redsky.target.com/v3/stores/nearby/" + url.PathEscape(query) + "?" + q.Encode()
+	data, err := GetRequest(u)
+	if err != nil {
+		return nil, errors.Wrap(err, "search stores")
+	}
+	var response []struct {
+		Locations []*LocationInfo `json:"locations"`
+	}
+	if err := json.Unmarshal(data, &response); err != nil {
+		return nil, errors.Wrap(err, "search stores")
+	}
+	if len(response) != 1 {
+		return nil, errors.New("search stores: unexpected result count")
+	}
+	return response[0].Locations, nil
 }
 
 // Geocodes searches a lat/lon pair and yields address
