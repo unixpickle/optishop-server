a67f300ba63aa71620a27617df215765f6d0698c
Alex Nichol <unixpickle@gmail.com>
2019-07-16 13:15:40 -0400

    start working on authentication

--- args.go
+++ args.go
@@ -4,10 +4,12 @@ import "flag"
 
 type Args struct {
 	AssetDir string
+	DataDir  string
 	Addr     string
 }
 
 func (a *Args) Add() {
 	flag.StringVar(&a.AssetDir, "assets", "assets", "web asset directory")
+	flag.StringVar(&a.DataDir, "data", "data", "data store directory")
 	flag.StringVar(&a.Addr, "addr", ":8080", "address to listen on")
 }
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"encoding/json"
 	"flag"
 	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/optishop-server/optishop/db"
 )
 
 func main() {
@@ -11,18 +16,42 @@ func main() {
 	args.Add()
 	flag.Parse()
 
-	server := &Server{AssetDir: args.AssetDir}
+	db, err := db.NewFileDB(args.DataDir)
+	essentials.Must(err)
+
+	server := &Server{
+		AssetDir: args.AssetDir,
+		DB:       db,
+	}
 	http.Handle("/", http.FileServer(http.Dir(args.AssetDir)))
-	http.HandleFunc("/login", server.HandleLogin)
+	http.HandleFunc("/login/submit", server.HandleLogin)
 	http.ListenAndServe(args.Addr, nil)
 }
 
 type Server struct {
 	AssetDir string
+	DB       db.DB
 }
 
 func (s *Server) HandleLogin(w http.ResponseWriter, r *http.Request) {
-	// TODO: this.
+	userID, err := s.DB.Login(r.FormValue("username"), r.FormValue("password"))
+	if err != nil {
+		http.Redirect(w, r, "/login?error="+url.QueryEscape(err.Error()), http.StatusSeeOther)
+		return
+	}
+	secret, err := s.DB.UserMetadata(userID, "secret")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name: "session",
+		Value: (url.Values{
+			"user":   []string{string(userID)},
+			"secret": []string{secret},
+		}).Encode(),
+		Expires: time.Now().Add(time.Hour * 24 * 30),
+	})
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func serveError(w http.ResponseWriter, r *http.Request, err error) {
