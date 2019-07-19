1d30a54ba317baaae596deb7c21d56c212a44973
Alex Nichol <unixpickle@gmail.com>
2019-07-19 13:09:25 -0400

    move more of stores.js to common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,70 @@
 const ENTER_KEY = 13;
 const ESCAPE_KEY = 27;
 
+class ListingPage {
+    constructor() {
+        this.addButton = document.getElementById('add-button');
+        this.itemList = document.getElementById('item-list');
+        this.emptyList = document.getElementById('empty-list');
+        this.addDialog = this.createAddDialog();
+
+        this.addButton.addEventListener('click', () => this.addDialog.open());
+        this.addDialog.onAdd = (item) => {
+            this.addItem(item).catch((err) => handleError(err));
+        }
+    }
+
+    updateData(items) {
+        if (items.length === 0) {
+            this.itemList.style.display = 'none';
+            this.emptyList.style.display = 'block';
+            return;
+        }
+        this.itemList.innerHTML = '';
+        items.forEach((item) => {
+            this.addListItem(item);
+        });
+        this.itemList.style.display = 'block';
+        this.emptyList.style.display = 'none';
+    }
+
+    addListItem(item) {
+        const element = this.createListItem(item);
+        element.addEventListener('click', () => {
+            this.selectedListItem(item);
+        });
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-button';
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.deleteItem(item).catch((err) => handleError(err));
+        });
+        element.appendChild(deleteButton);
+        this.itemList.appendChild(element);
+    }
+
+    createAddDialog() {
+        throw new Error('override this in a subclass');
+    }
+
+    createListItem(item) {
+        throw new Error('override this in a subclass');
+    }
+
+    selectedListItem(item) {
+        throw new Error('override this in a subclass');
+    }
+
+    addItem(item) {
+        throw new Error('override this in a subclass');
+    }
+
+    deleteItem(item) {
+        throw new Error('override this in a subclass');
+    }
+}
+
 class AddDialog {
     constructor() {
         this.element = document.getElementById('add-dialog');
--- assets/js/stores.js
+++ assets/js/stores.js
@@ -1,40 +1,28 @@
 (function () {
 
-    class StoresPage {
+    class StoresPage extends ListingPage {
         constructor() {
-            this.addButton = document.getElementById('add-button');
-            this.storesList = document.getElementById('stores-list');
-            this.noStores = document.getElementById('no-stores');
-            this.addStoreDialog = new AddStoreDialog();
-
-            this.addButton.addEventListener('click', () => this.addStoreDialog.open());
-            this.addStoreDialog.onAdd = (store) => this.addStore(store);
-
-            this.data = null;
+            super();
             this.updateData(window.STORES_DATA);
         }
 
-        updateData(data) {
-            this.data = data;
-
-            if (data.length === 0) {
-                this.storesList.style.display = 'none';
-                this.noStores.style.display = 'block';
-                return;
-            }
-            this.storesList.innerHTML = '';
-            data.forEach((datum) => {
-                this.addListItem(datum);
-            });
-            this.storesList.style.display = 'block';
-            this.noStores.style.display = 'none';
+        createAddDialog() {
+            return new AddStoreDialog();
         }
 
-        addStore(store) {
+        createListItem(store) {
+            return createListItem(store);
+        }
+
+        selectedListItem(store) {
+            window.open('/list?store=' + encodeURIComponent(store.id));
+        }
+
+        addItem(store) {
             const formData = 'source=' + encodeURIComponent(store.source) +
                 '&signature=' + encodeURIComponent(store.signature) +
                 '&data=' + encodeURIComponent(JSON.stringify(store.data));
-            fetch('/api/addstore', {
+            return fetch('/api/addstore', {
                 method: 'POST',
                 credentials: 'same-origin',
                 headers: {
@@ -43,46 +31,28 @@
                 body: formData,
             }).then((x) => x.json()).then((data) => {
                 if (data.error) {
-                    handleError(data.error);
-                    return;
+                    throw data.error;
                 }
                 store.id = data;
                 this.addListItem(store);
-            }).catch((err) => handleError(err));
+            });
         }
 
-        deleteStore(id) {
-            fetch('/api/removestore?store=' + encodeURIComponent(id), {
+        deleteItem(store) {
+            return fetch('/api/removestore?store=' + encodeURIComponent(store.id), {
                 credentials: 'same-origin',
             }).then((x) => x.json()).then((data) => {
                 if (data.error) {
-                    handleError(data.error);
-                    return;
+                    throw data.error;
                 }
                 this.updateData(data);
-            }).catch((err) => handleError(err));
-        }
-
-        addListItem(store) {
-            const element = createListItem(store);
-            element.addEventListener('click', () => {
-                window.open('/list?store=' + encodeURIComponent(store.id));
             });
-            const deleteButton = document.createElement('button');
-            deleteButton.className = 'delete-button';
-            deleteButton.textContent = 'Delete';
-            deleteButton.addEventListener('click', (e) => {
-                e.stopPropagation();
-                this.deleteStore(store.id);
-            });
-            element.appendChild(deleteButton);
-            this.storesList.appendChild(element);
         }
     }
 
     class AddStoreDialog extends AddDialog {
         fetchSearchResults(query) {
-            fetch('/api/storequery?query=' + encodeURIComponent(query), {
+            return fetch('/api/storequery?query=' + encodeURIComponent(query), {
                 credentials: 'same-origin',
             }).then((x) => x.json()).then((result) => {
                 if (result.error) {
