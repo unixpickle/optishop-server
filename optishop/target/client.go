d044e94dcb1116101e7962af895dbf8c8cd6ad2c
Alex Nichol <unixpickle@gmail.com>
2021-11-06 12:07:02 -0400

    track new VisitorID cookie

--- optishop/target/client.go
+++ optishop/target/client.go
@@ -3,17 +3,27 @@ package target
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
+
+	"github.com/pkg/errors"
 )
 
 type Client struct {
 	preloadedState map[string]interface{}
+	visitorID      string
 }
 
 func NewClient() (*Client, error) {
-	resp, err := GetRequest("https://www.target.com")
+	rawResp, err := GetRequestRaw("https://www.target.com")
 	if err != nil {
 		return nil, err
 	}
+	defer rawResp.Body.Close()
+	resp, err := ioutil.ReadAll(rawResp.Body)
+	if err != nil {
+		return nil, err
+	}
+
 	preloadMarker := []byte("window.__PRELOADED_STATE__= ")
 	idx := bytes.Index(resp, preloadMarker)
 	jsonPayload := resp[idx+len(preloadMarker):]
@@ -21,9 +31,17 @@ func NewClient() (*Client, error) {
 	jsonPayload = bytes.ReplaceAll(jsonPayload, []byte("new Set([])"), []byte("[]"))
 	var state map[string]interface{}
 	if err := json.NewDecoder(bytes.NewReader(jsonPayload)).Decode(&state); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "new store source")
 	}
-	return &Client{preloadedState: state}, nil
+	for _, cookie := range rawResp.Cookies() {
+		if cookie.Name == "visitorId" {
+			return &Client{
+				preloadedState: state,
+				visitorID:      cookie.Value,
+			}, nil
+		}
+	}
+	return nil, errors.New("new store source: no visitorId cookie set")
 }
 
 // Key gets the key field for various requests.
@@ -38,4 +56,9 @@ func (c *Client) Key() string {
 	}
 	data, _ := firefly["apiKey"].(string)
 	return data
+}
+
+// VisitorID gets the visitorId cookie.
+func (c *Client) VisitorID() string {
+	return c.visitorID
 }
